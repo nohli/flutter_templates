875c4eb8221282d5ec939263c40f2901be62ee32
Joachim Nohl <43643339+nohli@users.noreply.github.com>
2026-09-09 04:58:53 +0400

    Share the fitness section scaffold

--- lib/fitness_app/training/training_screen.dart
+++ lib/fitness_app/training/training_screen.dart
@@ -1,264 +1,33 @@
 import 'package:flutter/material.dart';
 
-import '../bottom_navigation_view/bottom_bar_view.dart';
-import '../fitness_app_theme.dart';
 import '../ui_view/area_list_view.dart';
+import '../ui_view/fitness_section_scaffold.dart';
 import '../ui_view/running_view.dart';
-import '../ui_view/sample_date_header.dart';
 import '../ui_view/title_view.dart';
 import '../ui_view/workout_view.dart';
 
-class TrainingScreen extends StatefulWidget {
+class TrainingScreen extends StatelessWidget {
   const TrainingScreen({required this.animation, super.key});
 
   final Animation<double> animation;
 
   @override
-  State<TrainingScreen> createState() => _TrainingScreenState();
-}
-
-class _TrainingScreenState extends State<TrainingScreen> {
-  final _sections = <Widget>[];
-  var _topBarOpacity = 0.0;
-
-  late final ScrollController _scrollController;
-  late final Animation<double> _topBarAnimation;
-  @override
-  void initState() {
-    super.initState();
-    _scrollController = ScrollController();
-    _topBarAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(
-        parent: widget.animation,
-        curve: const Interval(0, 0.5, curve: Curves.fastOutSlowIn),
-      ),
-    );
-    _addSections();
-    _scrollController.addListener(() {
-      if (_scrollController.offset >= 24) {
-        if (_topBarOpacity != 1.0) {
-          if (mounted) {
-            setState(() {
-              _topBarOpacity = 1.0;
-            });
-          }
-        }
-      } else if (_scrollController.offset <= 24 && _scrollController.offset >= 0) {
-        if (_topBarOpacity != _scrollController.offset / 24) {
-          if (mounted) {
-            setState(() {
-              _topBarOpacity = _scrollController.offset / 24;
-            });
-          }
-        }
-      } else if (_scrollController.offset <= 0) {
-        if (_topBarOpacity != 0.0) {
-          if (mounted) {
-            setState(() {
-              _topBarOpacity = 0.0;
-            });
-          }
-        }
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
-  }
-
-  void _addSections() {
-    const count = 5;
-
-    _sections.add(
-      TitleView(
-        title: 'Your program',
-        actionLabel: 'Details',
-        animation: Tween<double>(begin: 0.0, end: 1.0).animate(
-          CurvedAnimation(
-            parent: widget.animation,
-            curve: const Interval((1 / count) * 0, 1.0, curve: Curves.fastOutSlowIn),
-          ),
-        ),
-      ),
-    );
-
-    _sections.add(
-      WorkoutView(
-        animation: Tween<double>(begin: 0.0, end: 1.0).animate(
-          CurvedAnimation(
-            parent: widget.animation,
-            curve: const Interval((1 / count) * 1, 1.0, curve: Curves.fastOutSlowIn),
-          ),
-        ),
-      ),
-    );
-
-    _sections.add(
-      RunningView(
-        animation: Tween<double>(begin: 0.0, end: 1.0).animate(
-          CurvedAnimation(
-            parent: widget.animation,
-            curve: const Interval((1 / count) * 2, 1.0, curve: Curves.fastOutSlowIn),
-          ),
-        ),
-      ),
-    );
-
-    _sections.add(
-      TitleView(
-        title: 'Area of focus',
-        actionLabel: 'More',
-        animation: Tween<double>(begin: 0.0, end: 1.0).animate(
-          CurvedAnimation(
-            parent: widget.animation,
-            curve: const Interval((1 / count) * 3, 1.0, curve: Curves.fastOutSlowIn),
-          ),
-        ),
-      ),
-    );
-
-    _sections.add(
-      AreaListView(
-        mainScreenAnimation: Tween<double>(begin: 0.0, end: 1.0).animate(
-          CurvedAnimation(
-            parent: widget.animation,
-            curve: const Interval((1 / count) * 4, 1.0, curve: Curves.fastOutSlowIn),
-          ),
-        ),
-      ),
-    );
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;
-    final stackHeader = MediaQuery.textScalerOf(context).scale(1) >= 2;
-    return Material(
-      color: Theme.of(context).scaffoldBackgroundColor,
-      child: stackHeader
-          ? _buildStackedList(colors)
-          : Stack(
-              children: <Widget>[
-                _buildMainList(),
-                _buildAppBar(colors),
-                SizedBox(height: MediaQuery.of(context).padding.bottom),
-              ],
-            ),
+    const count = 5;
+    Animation<double> entrance(int index) => CurvedAnimation(
+      parent: animation,
+      curve: Interval(index / count, 1, curve: Curves.fastOutSlowIn),
     );
-  }
 
-  Widget _buildMainList() {
-    return ListView.builder(
-      controller: _scrollController,
-      padding: EdgeInsets.only(
-        top: AppBar().preferredSize.height + MediaQuery.of(context).padding.top + 24,
-        bottom: BottomBarView.contentPadding(context),
-      ),
-      itemCount: _sections.length,
-      itemBuilder: (BuildContext context, int index) {
-        return _sections[index];
-      },
-    );
-  }
-
-  Widget _buildStackedList(ColorScheme colors) {
-    return ListView.builder(
-      controller: _scrollController,
-      padding: EdgeInsets.only(bottom: BottomBarView.contentPadding(context)),
-      itemCount: _sections.length + 1,
-      itemBuilder: (BuildContext context, int index) {
-        return index == 0 ? _buildAppBar(colors) : _sections[index - 1];
-      },
-    );
-  }
-
-  Widget _buildAppBar(ColorScheme colors) {
-    final stackHeader = MediaQuery.textScalerOf(context).scale(1) >= 2;
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        AnimatedBuilder(
-          animation: widget.animation,
-          builder: (BuildContext context, _) {
-            return FadeTransition(
-              opacity: _topBarAnimation,
-              child: Transform(
-                transform: Matrix4.translationValues(0.0, 30 * (1.0 - _topBarAnimation.value), 0.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: colors.surface.withValues(alpha: _topBarOpacity),
-                    borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(32.0)),
-                    boxShadow: <BoxShadow>[
-                      BoxShadow(
-                        color: colors.shadow.withValues(alpha: 0.4 * _topBarOpacity),
-                        offset: const Offset(1.1, 1.1),
-                        blurRadius: 10.0,
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(height: MediaQuery.of(context).padding.top),
-                      Padding(
-                        padding: EdgeInsets.only(
-                          left: 16,
-                          right: 16,
-                          top: 16 - 8.0 * _topBarOpacity,
-                          bottom: 12 - 8.0 * _topBarOpacity,
-                        ),
-                        child: Flex(
-                          direction: stackHeader ? Axis.vertical : Axis.horizontal,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            if (stackHeader)
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  'Training',
-                                  textAlign: TextAlign.left,
-                                  style: TextStyle(
-                                    fontFamily: FitnessAppTheme.fontName,
-                                    fontWeight: FontWeight.w700,
-                                    fontSize: 22 + 6 - 6 * _topBarOpacity,
-                                    letterSpacing: 1.2,
-                                    color: colors.onSurface,
-                                  ),
-                                ),
-                              )
-                            else
-                              Expanded(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    'Training',
-                                    textAlign: TextAlign.left,
-                                    style: TextStyle(
-                                      fontFamily: FitnessAppTheme.fontName,
-                                      fontWeight: FontWeight.w700,
-                                      fontSize: 22 + 6 - 6 * _topBarOpacity,
-                                      letterSpacing: 1.2,
-                                      color: colors.onSurface,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            Padding(
-                              padding: EdgeInsets.only(left: 8, right: 8, top: stackHeader ? 8 : 0),
-                              child: const SampleDateHeader(),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            );
-          },
-        ),
+    return FitnessSectionScaffold(
+      title: 'Training',
+      animation: animation,
+      sections: <Widget>[
+        TitleView(title: 'Your program', actionLabel: 'Details', animation: entrance(0)),
+        WorkoutView(animation: entrance(1)),
+        RunningView(animation: entrance(2)),
+        TitleView(title: 'Area of focus', actionLabel: 'More', animation: entrance(3)),
+        AreaListView(mainScreenAnimation: entrance(4)),
       ],
     );
   }
